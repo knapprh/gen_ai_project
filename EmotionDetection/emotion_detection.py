ed76939144ad63fe08506ac036e113417e17b368
knappr2 <robert.h.knapp@medtronic.com>
2026-06-03 15:07:43 -0400

    First Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = myobj, headers=header)
+    json_response = json.loads(response.text)
+    response_formatted = json_response['emotionPredictions'][0]['emotion']
+
+    dominant_emotion_score = 0
+    dominant_emotion_name = ""
+    for emotion in response_formatted:
+        if response_formatted[emotion] > dominant_emotion_score:
+            dominant_emotion_name = emotion
+            dominant_emotion_score = response_formatted[emotion]
+
+    response_formatted['dominant_emotion'] = dominant_emotion_name
+    return response_formatted
